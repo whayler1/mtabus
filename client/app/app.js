c65e19a343104002862a2c54e9a1895fce05174e
Justin Worsdale <whayler1@gmail.com>
2016-05-06 16:58:45 -0700

    first commit

---
+++ client/app/app.js
@@ -0,0 +1,15 @@
+'use strict';
+
+angular.module('mtabusApp', [
+  'mtabusApp.constants',
+  'ngCookies',
+  'ngResource',
+  'ngSanitize',
+  'ui.router'
+])
+  .config(function($urlRouterProvider, $locationProvider) {
+    $urlRouterProvider
+      .otherwise('/');
+
+    $locationProvider.html5Mode(true);
+  });
