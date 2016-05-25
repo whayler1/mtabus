a197f9308295b002ee828a82b1a4dda05b525177
Justin Worsdale <whayler1@gmail.com>
2016-05-25 11:26:04 -0400

    prune bus stops so only stops in view are returned

--- client/components/bus-stops/bus-stops.controller.js
+++ client/components/bus-stops/bus-stops.controller.js
@@ -3,24 +3,7 @@
 angular.module('mtabusApp')
   .controller('BusStopsCtrl', function (
     $scope,
-    $log,
     busStops
   ) {
-
-    $log.log('%cbusStops:', 'background:yellow', $scope.stops);
-
     $scope.stops = busStops.stops;
-
-    // const gmap = map.gmap;
-    //
-    // const onBoundsChanged = _.debounce(() => {
-    //   console.log('map bounds updated!');
-    // }, 250);
-    //
-    // const mapBoundsChangeListener = gmap.addListener('bounds_changed', onBoundsChanged);
-
-    // $scope.$on('$destroy', () => {
-    //   $log.log('%cbus stops destroy', 'background:pink');
-    //   $window.google.maps.event.removeListener(mapBoundsChangeListener);
-    // });
   });
--- client/components/bus-stops/bus-stops.service.js
+++ client/components/bus-stops/bus-stops.service.js
@@ -18,8 +18,25 @@ angular.module('mtabusApp')
       busTime.getBusStops(center.lat(), center.lng(), map.getSpan()).then(
         res => {
           const newStops = res.data.data.stops;
-          console.log('SUCCESS!', newStops);
-          angular.copy(newStops, stops);
+          const stopsInView = [];
+
+          newStops.forEach(stop => {
+            const stopLat = stop.lat;
+            const stopLng = stop.lon;
+            const bounds = gmap.getBounds();
+            const northEast = bounds.getNorthEast();
+            const southWest = bounds.getSouthWest();
+
+            if(stopLat <= northEast.lat() &&
+                stopLat >= southWest.lat() &&
+                stopLng <= northEast.lng() &&
+                stopLng >= southWest.lng()) {
+                  stopsInView.push(stop);
+                }
+          });
+          console.log('stopsInView:', stopsInView);
+
+          angular.copy(stopsInView, stops);
         },
         res => console.log('FAIL!', res)
       );
