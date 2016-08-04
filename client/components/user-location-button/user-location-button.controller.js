2a7bba24382f48d3f4e4a9432d5c2c62769c86ea
Justin Worsdale <whayler1@gmail.com>
2016-08-03 22:08:46 -0400

    user location btn controller

--- client/components/user-location-button/user-location-button.controller.js
+++ client/components/user-location-button/user-location-button.controller.js
@@ -1,6 +1,18 @@
 'use strict';
 
 angular.module('mtabusApp')
-  .controller('UserLocationButtonCtrl', function ($scope) {
-    $scope.message = 'Hello';
+  .controller('UserLocationButtonCtrl', function (
+    $scope,
+    $window,
+    map
+  ) {
+
+    const hasGeoloc = (_.hasIn($window, 'navigator') && _.hasIn($window.navigator, 'geolocation'));
+
+    $scope.onClick = () => {
+      console.log('click');
+      if(hasGeoloc) {
+
+      }
+    };
   });
