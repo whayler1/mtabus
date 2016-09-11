075847d4695acc9c7acd350bfbac2fcd172a4c0f
Justin Worsdale <whayler1@gmail.com>
2016-09-11 16:07:02 -0400

    add page title and desciption to about page

--- client/app/about/about.js
+++ client/app/about/about.js
@@ -5,6 +5,10 @@ angular.module('mtabusApp')
     $stateProvider
       .state('about', {
         url: '/about',
+        data: {
+          pageTitle: 'About',
+          description: 'Never miss your bus again! NYC Bus Time Finder helps New Yorkers find upcoming departure times for all nearby MTA buses.'
+        },
         template: '<navbar></navbar><about></about>'
       });
   });
