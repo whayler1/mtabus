ed814500ac7861b1a707f775e80040f2303966a0
Justin Worsdale <whayler1@gmail.com>
2016-09-21 22:37:37 -0400

    first pass route page

--- client/app/route/route.js
+++ client/app/route/route.js
@@ -4,25 +4,32 @@ angular.module('mtabusApp')
   .config(function ($stateProvider) {
     $stateProvider
       .state('route', {
-        url: '/routes/:routeId/:shortName',
+        url: '/routes/:routeId',
         resolve: {
           route: ($q, $log, $stateParams, busTime) => busTime.getRoute($stateParams.routeId).then(
-            res => $q.when(res.data.data),
+            res => {
+              const { data } = res.data;
+              data.route = _.find(data.references.routes, {id: data.entry.routeId});
+              return $q.when(data);
+            },
             err => {
               $log.error('err!', err);
               return $q.when();
             }
-          ),
-          shortName: ($q, $stateParams) => $q.when($stateParams.shortName)
+          )
         },
         data: {
-          pageTitle: '{{ shortName }} Bus Route'
+          pageTitle: '{{ route.route.shortName }} Bus Route'
         },
         controller: ($scope, $rootScope, route) => {
           console.log('route', route);
           $scope.route = route;
           $rootScope.$emit('toggle-show-list-view', true);
         },
-        template: '<navbar></navbar><single-route route="route"></single-route>'
+        template: '<navbar></navbar><single-route route="route"></single-route><ui-view></ui-view>'
+      })
+      .state('route.group', {
+        url: '/:stopGroupId',
+        template: ''
       });
   });
--- client/components/single-route/single-route.controller.js
+++ client/components/single-route/single-route.controller.js
@@ -3,7 +3,5 @@
 angular.module('mtabusApp')
   .controller('SingleRouteCtrl', function ($scope) {
     console.log('SingleRouteCtrl:', $scope.route);
-    $scope.routeDetails = _.find($scope.route.references.routes, {id: $scope.route.entry.routeId});
-    console.log('routeDetails', $scope.routeDetails);
 
   });
