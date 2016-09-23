f65399d66db31a6d1f2e93581c5908e07698c566
Justin Worsdale <whayler1@gmail.com>
2016-09-22 21:53:14 -0400

    route links to bus stops

--- client/components/single-route/single-route.controller.js
+++ client/components/single-route/single-route.controller.js
@@ -3,5 +3,13 @@
 angular.module('mtabusApp')
   .controller('SingleRouteCtrl', function ($scope) {
     console.log('SingleRouteCtrl:', $scope.route);
-
+    const _stopGroups = $scope.route.entry.stopGroupings[0].stopGroups;
+    $scope.stopGroups = [];
+    _stopGroups.forEach(stopGroup => {
+      $scope.stopGroups.push({
+        name: stopGroup.name.name,
+        stops: stopGroup.stopIds.map(stopId => _.find($scope.route.references.stops, {id: stopId}))
+      });
+    });
+    console.log('stopGroups:', $scope.stopGroups);
   });
